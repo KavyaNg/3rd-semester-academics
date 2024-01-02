1973e47102af000d6f96e1849d0b11771163975d
KavyaNg <153646664+KavyaNg@users.noreply.github.com>
2024-01-02 15:13:01 +0530

    Add files via upload

--- wait.c
+++ wait.c
@@ -5,11 +5,13 @@
 int main()
 {
 	pid_t cpid;
-	if (fork() == 0)
+	if(fork()==0)
 		exit(0);
 	else
-		cpid = wait(NULL);
-	printf("parent pid = %d \n", getpid());
-	printf("child pid = %d \n", cpid);
+		cpid=wait(NULL);
+	printf("parent pid=%d\n",getpid());
+	printf("child pid=%d \n",cpid);
 	return 0;
 }
+
+
